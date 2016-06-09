23f970bd2439cece35dbe1fedc83b2ecc08a6989
Josephina <joisewurld@gmail.com>
2016-06-09 01:14:46 -0400

    formatting board

--- js/ccscript.js
+++ js/ccscript.js
@@ -1,1 +1,10 @@
 console.log('file is linked');
+
+for (var i =4; i > 0; i--){
+	$("body").append('<ul></ul>')
+	console.log('variable i is ' + i)
+		for (var k = 1; k < (i); k++){
+			console.log('variable k is' + k);
+			$('ul').append('<li><div></div></li>')
+		}
+}
